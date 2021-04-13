df975b68a2152c9a0962e7fc0f88ae9e508f3665
Akash <akash.ahire@cuelogic.com>
2021-04-13 12:32:37 +0530

    sso added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
+import { SocialAuthService } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider, MicrosoftLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+  user: SocialUser | undefined;
+  loggedIn!: boolean;
+  GoogleLoginProvider = GoogleLoginProvider;
+  constructor(private authService: SocialAuthService, private route: ActivatedRoute) {
+   }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      console.log('user', user);
+      console.log(user.authToken);
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+  });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithMicrosoft(): void {
+    this.authService.signIn(MicrosoftLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  refreshGoogleToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  MicrosoftLoginProvider,
+} from 'angularx-social-login';
+
+const googleLoginOptions = {
+  scope: 'profile email https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.readonly'
+};
+const microsoftOption = {
+  scopes: ['openid', 'email', 'profile', 'User.Read', 'Calendars.read']
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'GOOGLE_CLIENT_ID',
+              googleLoginOptions
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId')
+          },
+          {
+            id: MicrosoftLoginProvider.PROVIDER_ID,
+            provider: new MicrosoftLoginProvider('MICROSOFT_CLIENT_ID', microsoftOption),
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
